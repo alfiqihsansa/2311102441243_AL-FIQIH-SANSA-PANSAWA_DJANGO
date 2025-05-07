13dd05a1125663193445f9690ced8a880d4458a7
alfiqihsansa <alfiqihsansa@gmail.com>
2025-05-07 23:31:48 +0800

    Progress Template (Home)

--- berita/models.py
+++ berita/models.py
@@ -1,6 +1,11 @@
+import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
+
 # Create your models here.
+x = datetime.datetime.now()
 
 class Kategori(models.Model):
     nama = models.CharField(max_length=100)
@@ -11,16 +16,22 @@ class Kategori(models.Model):
     class Meta:
         verbose_name_plural = "1. Kategori"
 
-
 class Artikel(models.Model):
     judul = models.CharField(max_length=255)
     isi = models.TextField(blank=True, null=True)
     kategori = models.ForeignKey(Kategori, on_delete=models.SET_NULL, blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.PROTECT)
-    thumbnail = models.ImageField(upload_to='artikel', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='artikel',blank=True, null=True)
+
+    slug = models.SlugField(max_length=225, unique=True, blank=True, null=True)
 
     def __str__(self):
         return self.judul
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(f"{x.year}-{x.month}-{x.day}-{self.judul}")
+        super(Artikel, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = "2. Artikel"
